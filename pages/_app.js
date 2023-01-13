e23ddf840a03c2dc24dcdb18bd353a688cfbd6d0
coldraclette <timo.taennler@protonmail.com>
2023-01-13 15:24:56 +0200

    font

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,9 @@ import "../styles/globals.css";
 import localFont from "@next/font/local";
 import { Layout } from "../components/layout";
 
-const customfont = localFont({ src: "./font/UniversLTStd.otf" });
+const customfont = localFont({ src: "./font/univers.ttf" });
+
+// const customfont = Roboto({ subsets: ["latin"], weight: ["400"] });
 
 function MyApp({ Component, pageProps }) {
   return (
