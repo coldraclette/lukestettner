b0eebf1a9967a6346e684eac9afc954811f49e48
coldraclette <timo.taennler@protonmail.com>
2023-01-19 15:18:45 +0200

    some ui changes, added font implementation

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,9 @@
 import "../styles/globals.css";
-import localFont from "@next/font/local";
 import { Layout } from "../components/layout";
-
-const customfont = localFont({ src: "./font/univers.ttf" });
-
-// const customfont = Roboto({ subsets: ["latin"], weight: ["400"] });
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className={customfont.className}>
+    <div>
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,17 @@ import { Projects } from "../components/projects";
 import { Navigation } from "../components/navigation";
 import { getProjectsData, getSiteSettings } from "../lib/api";
 import Head from "next/head";
+import localFont from "@next/font/local";
+import { Public_Sans } from "@next/font/google";
+import { Old_Standard_TT } from "@next/font/google";
+
+const fontUnivers = localFont({ src: "./font/univers.ttf" });
+const fontPublicSans = Public_Sans({ subsets: ["latin"], weight: ["400"] });
+const fontOldSandardTT = Old_Standard_TT({ subsets: ["latin"], weight: ["400"] });
 
 export default function Home({ projects, projectTitleAndId, siteSettings }) {
-  const { siteTitle, siteDescription, backgroundColor, fontColor, menuString } = siteSettings;
+  const { siteTitle, siteDescription, backgroundColor, fontColor, menuString, fontFamily } =
+    siteSettings;
   const [artworkCloseClicked, setArtworkCloseClicked] = useState({
     clicked: false,
     project: null,
@@ -24,8 +32,22 @@ export default function Home({ projects, projectTitleAndId, siteSettings }) {
     setArtworkCloseClicked({ clicked: true, project: currentProject.id });
   };
 
+  const applyFontFamily = () => {
+    if (fontFamily === "fontPublicSans") {
+      return fontPublicSans.className;
+    } else if (fontFamily === "fontOldStandardTT") {
+      return fontOldSandardTT.className;
+    } else {
+      return fontUnivers.className;
+    }
+  };
+
   return (
-    <div id="main" style={{ backgroundColor: backgroundColor.hex, color: fontColor.hex }}>
+    <div
+      id="main"
+      className={applyFontFamily()}
+      style={{ backgroundColor: backgroundColor.hex, color: fontColor.hex }}
+    >
       <Head>
         <title>{siteTitle}</title>
         <meta name="description" content={siteDescription} />
