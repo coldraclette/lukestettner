73fd7a400f8fc174b0316ccf7aa578addef3fa96
Timo Tännler <timo.taennler@netnode.ch>
2024-02-12 17:11:54 +0200

    Update site description in site.config.ts

--- src/app/(site)/site.config.ts
+++ src/app/(site)/site.config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   title: 'Luke Stettner',
   description:
-    'Leven Betts Studio - Created in 1997 by David Leven and Stella Betts, Leven Betts Studio is a New York City based architectural practice committed to the exploration of contemporary architectural and urban ideas in both practical and theoretical projects.',
+    "Luke Stettner explores a range of experimental strategies in his photography and sculpture. He uses highly technical processes to create images and juxtaposes them with avant-garde language and concrete poetry. Stettner's work is invested in poetics, employing words to evoke both form and feeling—while still acknowledging that the meaning of any image is made by the viewer.",
   siteUrl: 'https://lukestettner.com',
   siteUrlShort: 'lukestettner.com',
 };
